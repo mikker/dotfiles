8b09014c3d2e2e3bc53720a5c6280c341a7022a0
Mikkel Malmberg <mikkelm@gmail.com>
2009-04-17 13:30:38 +0200

    a little something for vim

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-rm -rf ~/.profile ~/.bashrc ~/.gitconfig ~/.gemrc ~/.irbrc ~/.screenrc
 ln -fs `pwd`/bashrc ~/.bashrc
 ln -fs ~/.bashrc ~/.profile
 ln -fs `pwd`/gemrc ~/.gemrc
 ln -fs `pwd`/irbrc ~/.irbrc
 ln -fs `pwd`/gitconfig ~/.gitconfig
 ln -fs `pwd`/screenrc ~/.screenrc
+ln -fs `pwd`/vimrc ~/.vimrc
