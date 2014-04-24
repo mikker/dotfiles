e9bc4cdba703e8007c82ef1a55c5703588b60c8e
Mikkel Malmberg <mikkelmalmberg@me.com>
2014-04-24 16:20:21 +0200

    updated prompt again

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,27 @@
-autoload -U promptinit && promptinit
-prompt pure
+autoload -Uz vcs_info
+ 
+zstyle ':vcs_info:*' stagedstr '%F{28}●'
+zstyle ':vcs_info:*' unstagedstr '%F{11}●'
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
+zstyle ':vcs_info:*' enable git svn
+
+parse_ssh_connection() {
+  [[ -n $SSH_CONNECTION ]] && echo "%n@%m "
+}
+
+precmd () {
+  if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
+    zstyle ':vcs_info:*' formats ' [%F{green}%b%c%u%F{blue}]'
+  } else {
+    zstyle ':vcs_info:*' formats ' [%F{green}%b%c%u%F{red}●%F{blue}]'
+  }
+
+  vcs_info
+}
+
+setopt prompt_subst
+
+PROMPT='${parse_ssh_connection}%c %(?/%F{blue}/%F{red})$%f '
+RPROMPT='%F{blue}${vcs_info_msg_0_}%f'
 
