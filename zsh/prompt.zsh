89152e4189f291d0d499da05a5705e6bdbefc155
Mikkel Malmberg <mikkel@brnbw.com>
2017-03-30 09:43:35 +0200

    🍖

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,12 +1,12 @@
 autoload -Uz vcs_info
 
-local mark='.'
+local mark='•'
 
 zstyle ':vcs_info:*' stagedstr "%F{28}$mark"
 zstyle ':vcs_info:*' unstagedstr "%F{11}$mark"
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
-zstyle ':vcs_info:*' enable git svn
+zstyle ':vcs_info:*' enable git
 
 parse_ssh_connection() {
   [[ -n $SSH_CONNECTION ]] && echo "%n@%m "
@@ -22,6 +22,7 @@ precmd () {
   vcs_info
 }
 
-PROMPT='$(parse_ssh_connection)%c %(1j.%F{magenta}[%j]%f.)%F{blue}$%f '
+PROMPT='
+$(parse_ssh_connection)%F{blue}%c%f %(1j.%F{magenta}[%j]%f.)%F{black}▸%f '
 RPROMPT='%F{blue}${vcs_info_msg_0_}%f'
 
