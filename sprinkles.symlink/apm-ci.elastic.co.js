48568be0d5683a8d13c0f35d1fd84821a30b455f
Mikkel Malmberg <mikkel@brnbw.com>
2021-04-21 11:43:11 +0200

    📡

--- sprinkles.symlink/apm-ci.elastic.co.js
+++ sprinkles.symlink/apm-ci.elastic.co.js
@@ -1,35 +1,35 @@
-(function () {
-  let reran = false;
+// (function () {
+//   let reran = false;
 
-  apply();
+//   apply();
 
-  function apply() {
-    const output = document.querySelector(".console-output");
+//   function apply() {
+//     const output = document.querySelector(".console-output");
 
-    if (output) {
-      const lines = output.innerHTML.split("\n");
+//     if (output) {
+//       const lines = output.innerHTML.split("\n");
 
-      let script = false;
-      const replaced = lines.map(line => {
-        if (line.match(/SCRIPT EXECUTION BEGIN/)) {
-          script = true;
-        } else if (line.match(/SCRIPT EXECUTION END/)) {
-          script = false;
-        }
+//       let script = false;
+//       const replaced = lines.map(line => {
+//         if (line.match(/SCRIPT EXECUTION BEGIN/)) {
+//           script = true;
+//         } else if (line.match(/SCRIPT EXECUTION END/)) {
+//           script = false;
+//         }
 
-        return script ? paint(line, "black") : paint(line, "lightgray");
-      });
+//         return script ? paint(line, "black") : paint(line, "lightgray");
+//       });
 
-      output.innerHTML = replaced.join("\n");
-    } else {
-      if (!reran) {
-        setTimeout(apply, 250);
-        reran = true;
-      }
-    }
-  }
+//       output.innerHTML = replaced.join("\n");
+//     } else {
+//       if (!reran) {
+//         setTimeout(apply, 250);
+//         reran = true;
+//       }
+//     }
+//   }
 
-  function paint(line, color) {
-    return `<span style='color: ${color}'>${line}</span>`;
-  }
-})()
+//   function paint(line, color) {
+//     return `<span style='color: ${color}'>${line}</span>`;
+//   }
+// })()
