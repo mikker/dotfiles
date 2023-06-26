56d04cc29a1de5c7438a9c4a8dc1946537aeeee0
Mikkel Malmberg <mikkel@brnbw.com>
2023-06-26 11:42:25 +0200

    🧹

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -3,6 +3,7 @@ bindkey "\e[3~" delete-char
 # up-down searches history
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
+
 bindkey '^[b' backward-word
 bindkey '^[f' forward-word
 
